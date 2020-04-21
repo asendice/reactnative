ad46f2b113a6f13bd7af6dd87c7dd5e15b2c0f4c
Dylan Travis <dylan.travis23@outlook.com>
2020-04-20 23:53:13 -0700

    Components Part 2

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -9,9 +9,10 @@ function Directory(props) {
 
         return(
             <ListItem
-            title={item.name}
-            subtitle={item.description}
-            leftAvatar={{source: require('./images/react-lake.jpg')}}
+                title={item.name}
+                subtitle={item.description}
+                onPress={() => props.onPress(item.id)}
+                leftAvatar={{source: require('./images/react-lake.jpg')}}
         />
         );
 
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import { View } from 'react-native';
 import { CAMPSITES } from '../shared/campsites';
+
 
 // Main will be a container component that will be the parent to presentational components
 class Main extends Component {
     constructor(props){
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            selectedCampsite: null
         };
     }
 
+    onCampsiteSelect(campsiteId) {
+        this.setState({selectedCampsite: campsiteId});
+    }
+
     render() {
-        return <Directory campsites={this.state.campsites} />;
+        return(
+            <View style={{flex: 1}}>
+                <Directory 
+                    campsites={this.state.campsites} 
+                    onPress={campsiteId => this.onCampsiteSelect(campsiteId)}
+                />
+                <CampsiteInfo
+                    campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}
+                />
+            </View>
+        );
     }
 
 }
