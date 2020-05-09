2eb5e84625bc462e1f184d63320beca860e30588
Dylan Travis <dylan.travis23@outlook.com>
2020-05-08 21:48:14 -0700

    Persist Redux Store

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -26,10 +26,7 @@ function RenderCampsite(props) {
     const { campsite } = props;
 
     const view = React.createRef();
-
-
-
-                        //distance across x axis
+                        // dx distance across x axis
     const recognizeDrag = ({dx}) => (dx < -200) ? true: false;
 
     const panResponder = PanResponder.create({
